7e6a3ac644df4861702055fba1293abd4f7052ae
gmoein <gmoein@gmail.com>
2019-09-01 21:44:15 +0430

    Add files via upload

---
+++ react/precache-manifest.fca5f6181c7a60d427fe5cd3319fc6fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc766c3e3edcf4250ae2b9d5ec2f71e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5fb67946f127d3c0200",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1da6be7161761d1fc2c4",
+    "url": "/static/js/2.e524ec01.chunk.js"
+  },
+  {
+    "revision": "a5fb67946f127d3c0200",
+    "url": "/static/js/main.d7086935.chunk.js"
+  },
+  {
+    "revision": "79e7a6a5108818824b9e",
+    "url": "/static/js/runtime~main.6b26681a.js"
+  }
+]);
